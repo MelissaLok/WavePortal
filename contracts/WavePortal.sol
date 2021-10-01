b672c351b5a88f8147fa1123386cf5fbc24dd8a0
MelissaLok <melissalokvw@gmail.com>
2021-10-01 15:40:29 +1300

    initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+  constructor() {
+    console.log("hello hello, heres a very very basic smart contract!");
+    console.log("A smart is a piece of code that you can run on the blockchain");
+    console.log("A blockchain is just a network of computers that random ppl run");
+  }
+}
